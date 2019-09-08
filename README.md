97c6fc2d86fc50999e23ff0ec1a2174a5b4739ba
Aaron Anderson <aaronlynanderson@gmail.com>
2019-09-07 23:19:33 -0400

    :tada: progress from EthBoston

---
+++ README.md
@@ -0,0 +1,12 @@
+This is a version of Burner Wallet that will allow The Exchange Building to
+use many different custom tokens on the xDai chain with the Ching! PoS's
+around downtown.
+
+It will need to be hosted at uzb.cash in the near future.
+
+The feature that needs to be implemented now is:
+**_ it needs to alert the Ching! service that a transaction has been sent _**
+
+In order to assist in testing I have loaded a couple dozen IT tokens and a
+little gas on an address with the following private key:
+**_ 4a653f2dd3509a5e1fb0961330c65e80af1d094fbf8fc49c0b2bff01ba20f580 _**
---
+++ src/index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { xdai, ERC20Asset } from "@burner-wallet/assets";
+import BurnerCore from "@burner-wallet/core";
+import { InjectedSigner, LocalSigner } from "@burner-wallet/core/signers";
+import {
+  InfuraGateway,
+  InjectedGateway,
+  XDaiGateway
+} from "@burner-wallet/core/gateways";
+import Exchange from "@burner-wallet/exchange";
+import { xdaiBridge, uniswapDai } from "@burner-wallet/exchange/pairs";
+import BurnerUI from "@burner-wallet/ui";
+// import LegacyPlugin from "@burner-wallet/plugins/legacy";
+import ExchangeBldg from "./exchange-bldg";
+
+const exchangeBldg = new ERC20Asset({
+  id: "exch",
+  name: "EXCH",
+  network: "100",
+  address: "0x9b924c026325d307efb295108bafdfd29ecb3932"
+});
+
+const brass = new ERC20Asset({
+  id: "brass",
+  name: "BRASS",
+  network: "100",
+  address: "0x1b10e34165419f475923cf56ba2157eb96fd846a"
+});
+
+const it = new ERC20Asset({
+  id: "it",
+  name: "IT",
+  network: "100",
+  address: "0xd203308fff6ba932d7cdb8bbe12aea9d57204205"
+});
+
+const core = new BurnerCore({
+  signers: [new InjectedSigner(), new LocalSigner()],
+  gateways: [
+    new InjectedGateway(),
+    new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
+    new XDaiGateway()
+  ],
+  assets: [xdai, exchangeBldg, brass, it]
+});
+
+const exchange = new Exchange({
+  pairs: [xdaiBridge, uniswapDai]
+});
+
+const BurnerWallet = () => (
+  <BurnerUI core={core} plugins={[exchange, new ExchangeBldg()]} />
+);
+
+ReactDOM.render(<BurnerWallet />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

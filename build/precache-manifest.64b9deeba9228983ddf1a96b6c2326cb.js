8ec09e2f908fd99c364dda5f2402336747e117fa
Aaron Anderson <aaronlynanderson@gmail.com>
2019-09-25 15:31:55 -0500

    :sparkles: add build folder

---
+++ build/precache-manifest.64b9deeba9228983ddf1a96b6c2326cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f3ac8abfb6ce05d8de2e470db9d326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "319912891bef1a375670",
+    "url": "/static/css/2.77753a15.chunk.css"
+  },
+  {
+    "revision": "319912891bef1a375670",
+    "url": "/static/js/2.ede229b8.chunk.js"
+  },
+  {
+    "revision": "3ed1f313c19ebedf9626",
+    "url": "/static/js/main.b3c82db6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

8f0ee481612ca2d5b171af501ee2ddd9502336bb
Aaron Anderson <aaronlynanderson@gmail.com>
2019-10-04 14:31:59 -0500

    xdai only

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import { xdai, ERC20Asset } from "@burner-wallet/assets";
+import { xdai, dai, eth } from "@burner-wallet/assets";
 import BurnerCore from "@burner-wallet/core";
 import { InjectedSigner, LocalSigner } from "@burner-wallet/core/signers";
 import {
@@ -13,28 +13,7 @@ import Exchange from "@burner-wallet/exchange";
 import { xdaiBridge, uniswapDai } from "@burner-wallet/exchange/pairs";
 import BurnerUI from "@burner-wallet/ui";
 // import LegacyPlugin from "@burner-wallet/plugins/legacy";
-import ExchangeBldg from "./exchange-bldg";
-
-const exchangeBldg = new ERC20Asset({
-  id: "exch",
-  name: "EXCH",
-  network: "100",
-  address: "0x9b924c026325d307efb295108bafdfd29ecb3932"
-});
-
-const brass = new ERC20Asset({
-  id: "brass",
-  name: "BRASS",
-  network: "100",
-  address: "0x1b10e34165419f475923cf56ba2157eb96fd846a"
-});
-
-const it = new ERC20Asset({
-  id: "it",
-  name: "IT",
-  network: "100",
-  address: "0xd203308fff6ba932d7cdb8bbe12aea9d57204205"
-});
+import Ching from "./ching";
 
 const core = new BurnerCore({
   signers: [new InjectedSigner(), new LocalSigner()],
@@ -43,7 +22,7 @@ const core = new BurnerCore({
     new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
     new XDaiGateway()
   ],
-  assets: [xdai, exchangeBldg, brass, it]
+  assets: [xdai, dai, eth]
 });
 
 const exchange = new Exchange({
@@ -51,7 +30,7 @@ const exchange = new Exchange({
 });
 
 const BurnerWallet = () => (
-  <BurnerUI core={core} plugins={[exchange, new ExchangeBldg()]} />
+  <BurnerUI core={core} plugins={[exchange, new Ching()]} />
 );
 
 ReactDOM.render(<BurnerWallet />, document.getElementById("root"));

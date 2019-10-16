4757e9e3efb4d3bb23d31de5fea4777c824bbd56
Aaron Anderson <aaronlynanderson@gmail.com>
2019-10-16 15:01:17 -0500

    build for deployment

---
+++ build/precache-manifest.0df4bf4f18c4bd15b87ef46f6642da98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7924a8748cf29514a08b15749783e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "458fb5045bfaa7e41a85",
+    "url": "/static/css/2.5ce70b28.chunk.css"
+  },
+  {
+    "revision": "458fb5045bfaa7e41a85",
+    "url": "/static/js/2.cffdf130.chunk.js"
+  },
+  {
+    "revision": "11562308e69744e15f6d",
+    "url": "/static/js/main.1069dde4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
